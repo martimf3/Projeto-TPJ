ef316b01a1150e4515a6bc4a63e49446d91c3a15
João "Reed" Oliveira <77122709+reed-oliveira@users.noreply.github.com>
2023-01-03 16:52:27 +0000

    more shit

--- code/main.py
+++ code/main.py
@@ -69,7 +69,8 @@ class Game:
 # Pygame setup
 pygame.init()
 pygame.display.set_caption("Scraggle: Get the Diamond to Win!")
-#pygame_icon = pygame.image.load()
+game_icon = pygame.image.load('graphics/ui/icon.png')
+pygame.display.set_icon(game_icon)
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 game = Game()
@@ -80,8 +81,8 @@ while True:
 			pygame.quit()
 			sys.exit()
 	
-	screen.fill('grey')
 	game.run()
-
+	
 	pygame.display.update()
+	
 	clock.tick(60)
--- code/overworld.py
+++ code/overworld.py
@@ -53,11 +53,12 @@ class Overworld:
 		self.font = pygame.font.Font('graphics/ui/ARCADEPI.TTF', 20)
 		self.font_title = pygame.font.Font('graphics/ui/ARCADEPI.TTF', 50)
 		self.arrow_image = pygame.image.load('graphics/ui/arrow.png')
+		self.title_card = pygame.image.load("graphics/ui/title-card.png")
 		self.create_level = create_level
 		self.first_level = first_level
 
 		# Render the menu options
-		self.title = self.font_title.render("SCRAGGLE", True, self.WHITE)
+
 		self.option_1 = self.font.render(" NEW GAME ", True, self.WHITE)
 		self.option_2 = self.font.render(" QUIT ", True, self.WHITE)
 		self.text_y = 420
@@ -73,9 +74,10 @@ class Overworld:
 		# Fill the screen with black
 		screen.fill(pygame.Color(self.BLACK))
 
-		# Draw the menu options on the screen
-		title_rect = self.title.get_rect(center=(screen_width/2, screen_height/2))
-		screen.blit(self.title, title_rect)
+		# Draw the title card
+		screen.blit(self.title_card, (0, 0))
+
+		# Options
 
 		option1_rect = self.option_1.get_rect(center=(screen_width/2, screen_height/2))
 		screen.blit(self.option_1, (option1_rect.x,self.text_y))
@@ -84,7 +86,7 @@ class Overworld:
 		screen.blit(self.option_2, (option2_rect.x, self.text_y + 50))
 
 		# Draw arrow
-		screen.blit(self.arrow_image, (self.arrow_x,self.arrow_y))		
+		screen.blit(self.arrow_image, (self.arrow_x,self.arrow_y))
 
 		# Update the display
 		pygame.display.update()
@@ -102,19 +104,23 @@ class Overworld:
 	def Update(self):
 		# Check for user input
 		keys = pygame.key.get_pressed()
-
+		self.blip_sound = pygame.mixer.Sound('audio/effects/coin.wav')
 		if keys[pygame.K_RETURN] and self.option == 0:
 			self.create_level(self.first_level)
+			self.blip_sound.play()
 			print('enter pressed in 0')
 		elif keys[pygame.K_RETURN] and self.option == 1:
+			self.blip_sound.play()
 			pygame.QUIT
 			pygame.quit()
 			sys.exit()
 		elif keys[pygame.K_DOWN] and self.option == 0:
+			self.blip_sound.play()
 			self.option = 1
 			self.arrow_x += 75
 			self.arrow_y += 55
 		elif keys[pygame.K_UP] and self.option == 1:
+			self.blip_sound.play()
 			self.option = 0
 			self.arrow_x -= 75
 			self.arrow_y += -55
