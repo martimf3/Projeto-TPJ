53d8c18762a1dd32e968e92482b4562cb93d11ca
martimf3 <martimmrcf1@gmail.com>
2023-01-03 19:35:32 +0000

    level changes

--- code/game_data.py
+++ code/game_data.py
@@ -2,7 +2,7 @@ level_0 = {
 		'terrain': 'levels/0/level_0_terrain.csv',
 		'coins':'levels/0/level_0_coins.csv',
 		'enemies':'levels/0/level_0_enemies.csv',
-		#'diamond':'levels/0/level_0_diamond.csv',
+		'diamond':'levels/0/level_0_diamond.csv',
 		'constraints':'levels/0/level_0_constraints.csv',
 		'player': 'levels/0/level_0_player.csv',
 		'grass': 'levels/0/level_0_grass.csv'}
@@ -10,7 +10,7 @@ level_1 = {
 		'terrain': 'levels/1/level_1_terrain.csv',
 		'coins':'levels/1/level_1_coins.csv',
 		'enemies':'levels/1/level_1_enemies.csv',
-		#'diamond':'levels/1/level_1_diamond.csv',
+		'diamond':'levels/1/level_1_diamond.csv',
 		'constraints':'levels/1/level_1_constraints.csv',
 		'player': 'levels/1/level_1_player.csv',
 		'grass': 'levels/1/level_1_grass.csv'}
